0318a3cf5948f3ba90e15936bd221bfb2830e201
AudreyMeredith <ameredith@ucsd.edu>
2025-05-09 19:37:48 -0700

    commit

--- meta/main.js
+++ meta/main.js
@@ -46,6 +46,39 @@ async function loadData() {
 //   // You can now use `data` safely here
 // })();
 
-let data = await loadData();
-let commits = processCommits(data);
-console.log(commits)
+function renderCommitInfo(data, commits) {
+    // Create the dl element
+    const dl = d3.select('#stats').append('dl').attr('class', 'stats');
+  
+    // Add total LOC
+    dl.append('dt').html('Total <abbr title="Lines of code">LOC</abbr>');
+    dl.append('dd').text(data.length);
+  
+    // Add total commits
+    dl.append('dt').text('Total commits');
+    dl.append('dd').text(commits.length);
+  
+    dl.append('dt').text('Number of files');
+    dl.append('dd').text(d3.group(data, d => d.file).size);
+
+    dl.append('dt').text('Max file length');
+    dl.append('dd').text(d3.max(d3.rollups(data, v => d3.max(v, d => +d.line), d => d.file), d => d[1]));
+
+    console.log("Example text values:", data.slice(0, 5).map(d => d.text));
+
+    dl.append('dt').text('Average line length');
+    dl.append('dd').text(d3.mean(data, d => d.length).toFixed(2));
+
+    dl.append('dt').text('Longest line length');
+    dl.append('dd').text(d3.max(data, d => d.length));
+
+  }
+  
+  (async () => {
+    const data = await loadData();
+    console.log("Example text field values:", data.slice(0, 5).map(d => d.text));
+    const commits = processCommits(data);
+    renderCommitInfo(data, commits);
+  })();
+  
+
